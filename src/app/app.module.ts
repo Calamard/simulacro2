81a54055ad192f7ec74574c2b4d4199bc6ee2ec4
Jorge <jcalquin.sg@gmail.com>
2019-12-09 16:24:16 -0300

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { EleccionComponent } from './components/eleccion/eleccion.component';
+import { LoginPComponent } from './components/login-p/login-p.component';
+import { LoginAComponent } from './components/login-a/login-a.component';
+import { InicioPComponent } from './components/inicio-p/inicio-p.component';
+import { NotasComponent } from './components/notas/notas.component';
+import { CajaComponent } from './components/caja/caja.component';
 
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'asd', component: LoginComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    EleccionComponent,
+    LoginPComponent,
+    LoginAComponent,
+    InicioPComponent,
+    NotasComponent,
+    CajaComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
